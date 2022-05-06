321ccda8acd66a7406f7e5f78f88fe3b78ce191c
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-05-06 19:47:20 +0300

    Update Program.cs

--- 01. Day of Week/Program.cs
+++ 01. Day of Week/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace _01._Day_of_Week
 {
@@ -6,33 +6,47 @@ namespace _01._Day_of_Week
     {
         static void Main(string[] args)
         {
-            int input = int.Parse(Console.ReadLine());
-            int numOfDay = input - 1;
-            string day = string.Empty;
+            int numOfDay = int.Parse(Console.ReadLine());
 
             string[] daysOfWeak = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday",
             "Saturday", "Sunday"};
-            
 
-            if (input > 0 && input <= 7)
+            if (numOfDay > 0 && numOfDay <= 7)
             {
-                for (int i = 0; i <= daysOfWeak.Length - 1; i++)
-                {
-                    int index = i;
-
-                    if (index == numOfDay)
-                    {
-                        day = daysOfWeak[i];
-                        Console.WriteLine(day);
-                    }
-
-                }
+                Console.WriteLine(daysOfWeak[numOfDay - 1]);
             }
             else
             {
                 Console.WriteLine("Invalid day!");
             }
             
+            //int input = int.Parse(Console.ReadLine());
+            //int numOfDay = input - 1;
+            //string day = string.Empty;
+
+            //string[] daysOfWeak = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday",
+            //"Saturday", "Sunday"};
+
+            //if (input > 0 && input <= 7)
+            //{
+            //    for (int i = 0; i <= daysOfWeak.Length - 1; i++)
+            //    {
+            //        int index = i;
+
+            //        if (index == numOfDay)
+            //        {
+            //            day = daysOfWeak[i];
+            //            Console.WriteLine(day);
+            //        }
+
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid day!");
+            //}
+
+
         }
     }
 }
