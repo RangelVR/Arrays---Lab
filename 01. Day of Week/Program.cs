0b0ba5a1edbf447fe157023defa7e2924e910663
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-05-06 19:34:32 +0300

    Add files via upload

---
+++ 01. Day of Week/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _01._Day_of_Week
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            int numOfDay = input - 1;
+            string day = string.Empty;
+
+            string[] daysOfWeak = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday",
+            "Saturday", "Sunday"};
+            
+
+            for (int i = 0; i <= daysOfWeak.Length - 1; i++)
+            {
+                int index = i;
+
+                if (index == numOfDay)
+                {
+                    day = daysOfWeak[i];
+                    Console.WriteLine(day);
+                }
+                
+            }
+            
+        }
+    }
+}
