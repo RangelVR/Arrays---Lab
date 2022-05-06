a85b7f91e9768b4671502ff1129182e55b873fba
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-05-06 20:24:13 +0300

    Update Program.cs

--- 02. Print Numbers in Reverse Order/Program.cs
+++ 02. Print Numbers in Reverse Order/Program.cs
@@ -14,22 +14,13 @@ namespace _02._Print_Numbers_in_Reverse_Order
                 numbers[i] = int.Parse(Console.ReadLine());
                  
             }
-            Array.Reverse(numbers);
-
-            Console.WriteLine(string.Join(" ", numbers));
-            
-            //int n = int.Parse(Console.ReadLine());
-            //int[] numbers = new int[n];
-
-            //for (int i = 0; i < n; i++)
-            //{
-            //    numbers[i] = int.Parse(Console.ReadLine());
-                 
-            //}
             //for (int i = numbers.Length - 1; i >= 0; i--)
             //{
             //    Console.Write($"{numbers[i]} ");
             //}
+            Array.Reverse(numbers);
+
+            Console.WriteLine(string.Join(" ", numbers));
         }
     }
 }
