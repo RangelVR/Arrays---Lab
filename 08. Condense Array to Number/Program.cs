7baaf4a28b39ffcf9ea86deea609b52634c872df
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-05-08 20:28:42 +0300

    Add files via upload

---
+++ 08. Condense Array to Number/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace _08._Condense_Array_to_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int counter = arr.Length - 1;
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = 0; j < counter; j++)
+                {
+                    arr[j] = arr[j] + arr[j + 1];
+                }
+                counter--;
+                
+            }
+
+            Console.WriteLine(arr[0]);
+        }
+    }
+}
