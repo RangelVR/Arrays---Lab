ac161d446361aeda6acf15e76f2b51e12a31c2b5
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-05-08 15:10:28 +0300

    Add files via upload

---
+++ 04. Reverse Array of Strings/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Reverse_Array_of_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            //Array.Reverse(input);
+            //Console.WriteLine(string.Join(" ", input));
+
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                Console.Write($"{input[i]} ");
+            }
+        }
+    }
+}
