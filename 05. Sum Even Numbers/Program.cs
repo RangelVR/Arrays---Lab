75519679c182fa90a7c7427f53b6cb9b3cf6ab47
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-05-08 15:18:38 +0300

    Add files via upload

---
+++ 05. Sum Even Numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._Sum_Even_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int sum = 0;
+
+            for (int i = 0; i <= num.Length - 1; i++)
+            {
+                if (num[i] % 2 == 0)
+                {
+                    sum += num[i];
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
