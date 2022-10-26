079c67b67b6142ce9527fe3fff778e36bb3a2f53
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-10-26 21:59:49 +0300

    Update Program.cs

--- 07. Equal Arrays/Program.cs
+++ 07. Equal Arrays/Program.cs
@@ -33,6 +33,26 @@ namespace _07._Equal_Arrays
             }
 
             Console.WriteLine($"Arrays are identical. Sum: {sum}");
+            
+            //bool isEqual = arr1.SequenceEqual(arr2);
+            ////  bool isEqual = Enumerable.SequenceEqual(arr1, arr2);
+            //if (isEqual)
+            //{
+            //    Console.WriteLine($"Arrays are identical. Sum: {arr1.Sum()}");
+            //}
+            //else
+            //{
+            //    int difIndex = 0;
+            //    for (int i = 0; i < arr1.Length; i++)
+            //    {
+            //        if (arr1[i] != arr2[i])
+            //        {
+            //            difIndex = i;
+            //            Console.WriteLine($"Arrays are not identical. Found difference at {difIndex} index");
+            //            break;
+            //        }
+            //    }
+            //}
         }
     }
 }
