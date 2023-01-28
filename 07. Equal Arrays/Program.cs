f84fdc054ce7e29b996f71af28aa51fd96c41459
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-01-28 21:02:36 +0200

    Update Program.cs

--- 07. Equal Arrays/Program.cs
+++ 07. Equal Arrays/Program.cs
@@ -1,57 +1,81 @@
-using System;
-using System.Linq;
+int[] arr1 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int[] arr2 = Console.ReadLine().Split().Select(int.Parse).ToArray();
 
-namespace demo
+for (int i = 0; i < arr1.Length; i++)
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            int[] arr1 = Console.ReadLine()
-                .Split(" ")
-                .Select(int.Parse)
-                .ToArray();
-            int[] arr2 = Console.ReadLine()
-                .Split(" ")
-                .Select(int.Parse)
-                .ToArray();
-
-            int sum = 0;
-
-            for (int i = 0; i < arr1.Length; i++)
-            {
-                if (arr1[i] == arr2[i])
-                {
-                    sum += arr1[i];
-                }
-                else
-                {
-                    Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
-                    return;
-                }
-            }
-            Console.WriteLine($"Arrays are identical. Sum: {sum}");
-            
-            
-            //bool isEqual = arr1.SequenceEqual(arr2);
-            ////  bool isEqual = Enumerable.SequenceEqual(arr1, arr2);
-            //if (isEqual)
-            //{
-            //    Console.WriteLine($"Arrays are identical. Sum: {arr1.Sum()}");
-            //}
-            //else
-            //{
-            //    int difIndex = 0;
-            //    for (int i = 0; i < arr1.Length; i++)
-            //    {
-            //        if (arr1[i] != arr2[i])
-            //        {
-            //            difIndex = i;
-            //            Console.WriteLine($"Arrays are not identical. Found difference at {difIndex} index");
-            //            break;
-            //        }
-            //    }
-            //}
-        }
+	if (arr1[i] != arr2[i])
+	{
+        Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+        return;
     }
+	
 }
+
+Console.WriteLine($"Arrays are identical. Sum: {arr1.Sum()}");
+---------------------------------------------------
+
+//int[] arr1 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+//int[] arr2 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+//int sum = 0;
+
+//for (int i = 0; i < arr1.Length; i++)
+//{
+//	if (arr1[i] == arr2[i])
+//	{
+//		sum += arr1[i];
+//	}
+//	else
+//	{
+//      Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+//		return;
+//  }
+//}
+
+//Console.WriteLine($"Arrays are identical. Sum: {sum}");
+---------------------------------------------------
+
+//int[] arr1 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+//int[] arr2 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+//bool isEqual = Enumerable.SequenceEqual(arr1, arr2);
+
+//if (isEqual)
+//{
+//    Console.WriteLine($"Arrays are identical. Sum: {arr1.Sum()}");
+//}
+//else
+//{
+//	for (int i = 0; i < arr1.Length; i++)
+//	{
+//		if (arr1[i] != arr2[i])
+//		{
+//          Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+//			break;
+//      }
+//	}
+//}
+---------------------------------------------------
+
+//int[] arr1 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+//int[] arr2 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+//bool isEqual = true;
+//int sum = 0;
+
+//for (int i = 0; i < arr1.Length; i++)
+//{
+//	if (arr1[i] != arr2[i])
+//	{
+//		Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+//		isEqual = false;
+//		break;
+//	}
+
+//	sum += arr1[i];
+//}
+
+//if (isEqual)
+//{
+//    Console.WriteLine($"Arrays are identical. Sum: {sum}");
+//}
