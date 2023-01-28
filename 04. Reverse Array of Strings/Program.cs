a04be08676b8780def7662b1cb3a0c318c354646
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-01-28 19:59:03 +0200

    Update Program.cs

--- 04. Reverse Array of Strings/Program.cs
+++ 04. Reverse Array of Strings/Program.cs
@@ -1,3 +1,10 @@
 string[] arr = Console.ReadLine().Split().Reverse().ToArray();
-
 Console.WriteLine(string.Join(" ", arr));
+
+//string[] arr = Console.ReadLine().Split().ToArray();
+//arr = arr.Reverse().ToArray();
+//Console.WriteLine(string.Join(" ", arr));
+
+//string[] arr = Console.ReadLine().Split().ToArray();
+//Array.Reverse(arr);
+//Console.WriteLine(string.Join(" ", arr));
