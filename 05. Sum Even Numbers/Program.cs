d03b596251d9a464e7a15bb4f1e2fd8c562864e7
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-01-28 20:19:28 +0200

    Update Program.cs

--- 05. Sum Even Numbers/Program.cs
+++ 05. Sum Even Numbers/Program.cs
@@ -1,24 +1,30 @@
-using System;
-using System.Linq;
+int[] arr = Console.ReadLine().Split().Select(int.Parse).Where(x => x % 2 == 0).ToArray();
+int sum = arr.Sum();
+Console.WriteLine(sum);
+----------------------------------------------------------------------------------------------------
+    
+//int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
 
-namespace demo
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            int[] arr = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
-            int sum = 0;
+//int sum = 0; 
 
-            for (int i = 0; i < arr.Length; i++)
-            {
-                int n = arr[i];
-                if (n % 2 == 0)
-                {
-                    sum += n;
-                }
-            }
-            Console.WriteLine(sum);
-        }
-    }
-}
+//foreach (var number in arr.Where(x => x % 2 == 0))
+//{
+//    sum += number;
+//}
+
+//Console.WriteLine(sum);
+ ----------------------------------------------------------------------------------------------------
+    
+//int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+//int sum = 0;
+
+//for (int i = 0; i < arr.Length; i++)
+//{
+//	if (arr[i] % 2 == 0)
+//	{
+//		sum += arr[i];
+//	}
+//}
+
+//Console.WriteLine(sum);
